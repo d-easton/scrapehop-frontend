aae05fb333cf103d121a9476b05d103ea73a751e
David <d.easton@wustl.edu>
2020-07-26 20:04:26 -0400

    cleanup

--- src/app/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDom from 'react=dom';
-import App from './components/app/app';
-
-const appContainer = document.createElement('div');
-document.body.appendChild(appContainer);
-
-ReactDom.render(<App />, appContainer);
